165ab7be5fae4fbcaa1b1c205412b41f9d9eb883
overtorment <overtorment@gmail.com>
2023-09-17 18:48:55 +0100

    REL

---
+++ _next/static/LD-5jD_KckW1waaR6Mr3G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/201-f367db43ef95d8985a72.js","static/chunks/pages/index-9ae9b1c06c06925fefad.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
